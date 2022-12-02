f05a8a7750b846726dc814b7835d74409ef9b379
Ivan Miloslavov <miloslavovivan@gmail.com>
2022-12-02 14:34:38 -0500

    Optimized file reading to use a Seq

--- 2022/test.ml
+++ 2022/test.ml
@@ -1,8 +1,7 @@
 let square x = x * x;;
 
-let load_file filename = 
-	let ic: in_channel = open_in filename in
-	let rec gen_lines acc =
+let file_to_line_seq filename : string Seq.t = 
+	let unfold_fn ic =
 		try
 			let line = input_line ic in
 			let line = 
@@ -10,9 +9,17 @@ let load_file filename =
 				then String.sub line 0 (String.length line - 1)
 				else line
 			in
-			gen_lines (line :: acc)
+			Some (line, ic)
 		with
-		| End_of_file -> List.rev acc
+		| End_of_file -> None
 	in
-	gen_lines []
+	open_in filename
+	|> Seq.unfold unfold_fn
+	|> Seq.memoize
+;;
+
+let file_to_line_list filename = 
+	filename
+	|> file_to_line_seq 
+	|> List.of_seq
 ;;
