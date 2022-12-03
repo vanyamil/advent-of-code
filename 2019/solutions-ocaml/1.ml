ba54e386b60bfdde0af743b4a5c512836ca59801
Ivan Miloslavov <miloslavovivan@gmail.com>
2022-12-02 19:03:03 -0500

    Felt like 2019, day 1

---
+++ 2019/solutions-ocaml/1.ml
@@ -0,0 +1,38 @@
+(*
+	Year: 2019
+	Day: 1
+	Name: The Tyranny of the Rocket Equation
+*)
+
+(* Header *)
+
+#use "shared/utilities.ml" ;;
+
+let input_file = file_to_line_seq "2019/inputs/1.txt" ;;
+
+(* Main file *)
+
+let part1 = 
+	input_file
+	|> Seq.map int_of_string
+	|> Seq.map (fun x -> x / 3 - 2)
+	|> Seq.fold_left (+) 0
+;;
+
+let rec find_mass x =
+	let x' = x / 3 - 2 in
+	if x' <= 0
+	then 0
+	else x' + find_mass x'
+;;
+
+let part2 = 
+	input_file
+	|> Seq.map int_of_string
+	|> Seq.map find_mass
+	|> Seq.fold_left (+) 0
+;;
+
+(* Final print *)
+
+print_results part1 part2 ;;
